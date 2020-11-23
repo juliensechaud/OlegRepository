4b2cef3f4e61b12794d444c5ddedadbb74fe9f7f
Julien SECHAUD <julien.sechaud@happn.fr>
2020-11-23 11:58:40 +0100

    Add Xcode project

---
+++ OlegRepository/OlegRepository/OlegRepository.h
@@ -0,0 +1,18 @@
+//
+//  OlegRepository.h
+//  OlegRepository
+//
+//  Created by Julien Sechaud on 23/11/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OlegRepository.
+FOUNDATION_EXPORT double OlegRepositoryVersionNumber;
+
+//! Project version string for OlegRepository.
+FOUNDATION_EXPORT const unsigned char OlegRepositoryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OlegRepository/PublicHeader.h>
+
+
